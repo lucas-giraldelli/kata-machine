59c1c13f05dd5ef39b000a8705293e9124ea53d3
Lucas Giraldelli <lucas.santos@treez.io>
2023-05-28 15:11:02 -0400

    two crystals, bubble and binary

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,20 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
   let lo = 0;
   let hi = haystack.length;
 
-  for (let i = 0; lo < hi; i++) {
-    const m = Math.floor(lo + (hi -lo)/2);
-    const v = haystack[m];
+  do {
+    const mid = Math.floor(lo + (hi - lo)/2);
+    const value = haystack[mid];
 
-    if (v === needle) {
+    if (needle === value) {
       return true;
-    } else if (v > needle) {
-      hi = m;
+    } else if (needle < value) {
+      hi = mid;
     } else {
-      lo = m + 1;
+      lo = mid + 1;
     }
-  }
+
+  } while (lo < hi);
 
   return false;
 }
+
+const array = [1,5,6,11,12,15,25,30,45,77];
