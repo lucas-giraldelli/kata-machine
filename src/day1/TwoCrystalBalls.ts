fe0632970e7f5b4a7ae6013dd176d33f0d8ca570
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2025-04-28 22:02:05 -0400

    complete: two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+export default function two_crystal_balls(breaks: boolean[]) {
+    // you only have 2 crystal balls
+    // because you can break the first one
+    // jumping by sqrt root of n and then walk back finding it;
 
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    for (let j = 0; j <= jumpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
