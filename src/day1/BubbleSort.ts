9292e920c8842015db3decfdcd04a76051950cc9
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2025-04-28 23:48:16 -0400

    complete: bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
-export default function bubble_sort(arr: number[]): void {
+export default function bubble_sort(arr: number[]) {
+    let len = arr.length;
 
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let aux = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = aux;
+            }
+        }
+    }
 }
